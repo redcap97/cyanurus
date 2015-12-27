c90fea0b0c327cd0877dfdaa0ee2238670b21082
redcap97 <redcap97@gmail.com>
2015-12-27 16:19:01 +0900

    remove comments

--- src/kernel/syscall.c
+++ src/kernel/syscall.c
@@ -14,8 +14,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-/* FIXME: must check syscall arguments */
-
 #include "syscall.h"
 #include "system.h"
 #include "mmu.h"
